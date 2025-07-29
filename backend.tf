349bc39036fd2a0e48dbb1e8a8016049684d3278
Praveen Buddha <buddhwar@yahoo.com>
2025-07-28 20:54:41 -0400

    final version

--- backend.tf
+++ backend.tf
@@ -1,7 +1,7 @@
-# terraform {
-#   backend "s3" {
-#     bucket = "tfs3backend-test"
-#     key    = "vpc"
-#     region = "us-east-1"
-#   }
-# }
+terraform {
+  backend "s3" {
+    bucket = "tfs3backend-test"
+    key    = "vpc/terraform.tfstate"
+    region = "us-east-1"
+  }
+}
